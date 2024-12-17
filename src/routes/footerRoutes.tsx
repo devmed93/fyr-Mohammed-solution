b17b5349ef6434854132663075882fcc3ee0c466
devmed93 <el.moutaouakkil@hotmail.com>
2024-12-17 02:37:35 +0100

    feat: add currentChatPage route to footerRoutes configuration

--- src/routes/footerRoutes.tsx
+++ src/routes/footerRoutes.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { authPages } from '@/config/pages.config';
+import { authPages, medMindedAppPages } from '@/config/pages.config';
 import { TRoute } from '@/types/route.type';
 import DefaultFooterTemplate from '../templates/layouts/Footers/DefaultFooter.template';
 
 const footerRoutes: TRoute[] = [
 	{ path: authPages.loginPage.to, element: null },
+	{ path: medMindedAppPages.currentChatPage.to, element: null },
 	{ path: '/*', element: <DefaultFooterTemplate /> },
 ];
 
